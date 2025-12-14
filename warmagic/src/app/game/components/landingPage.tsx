2777a83a2b22db71fb19c6c22a13847e9a184240
Konrad Klautzsch <konradklautzsch@gmail.com>
2025-12-15 00:51:11 +0100

    I made a popover profile menu

--- warmagic/src/app/game/components/landingPage.tsx
+++ warmagic/src/app/game/components/landingPage.tsx
@@ -1,25 +1,29 @@
 export default function LandingPage() {
     return(
         <section
-        className="relative h-screen w-full bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url('/assets/background/landingsitebg.png')", fontFamily: "IsoCore" }}
+        className="relative h-screen w-screen"
+        style={{ fontFamily: "IsoCore" }}
         >
-            <div className="absolute top-6 left-6 text-white text-xl font-bold">
+            <div 
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat brightness-75"
+                style={{ backgroundImage: "url('/assets/background/dracula_castle_4k_background.png')" }}
+            ></div>
+            <div className="absolute top-[65px] left-[100px] text-white text-[27px] font-bold">
                 WarMagic
             </div>
-            <div className="absolute top-6 right-6 flex gap-4">
-                <button className="px-8 py-3 text-white  backdrop-blur" style={{ backgroundColor: "var(--custom-yellow)"}}>
-                    Login
+            <div className="absolute top-[65px] right-[100px] flex gap-4">
+                <button className="px-10 py-4 text-[17px] text-white transition-all duration-300 bg-[var(--custom-yellow)] hover:bg-[#ebbc6c] backdrop-blur">
+                    Sign in
                 </button>
-                <button className="px-8 py-3 text-white backdrop-blur" style={{ backgroundColor: "var(--custom-yellow)"}}>
+                <button className="px-10 py-4 text-[17px] text-white transition-all duration-300 bg-[var(--custom-yellow)] hover:bg-[#ebbc6c] backdrop-blur">
                     Sign Up
                 </button>
             </div>
 
-            <div className="absolute left-10 top-1/3 text-white">
+            <div className="absolute left-[150px] top-3/7 text-white">
                 <h1 className="text-9xl font-bold">WarMagic</h1>
                 <p className="text-3xl mt-2">Placeholder description</p>
-                <button className="mt-6 px-8 py-3 text-white" style={{ backgroundColor: "var(--custom-yellow)"}}>Play</button>
+                <button className="mt-8 px-10 py-4 text-[17px] text-white transition-all duration-300 bg-[var(--custom-yellow)] hover:bg-[#ebbc6c]">Play</button>
             </div>
         </section>
     )
--- warmagic/src/app/page.tsx
+++ warmagic/src/app/page.tsx
@@ -1,9 +1,11 @@
 import LandingPage from './game/components/landingPage';
+// import MainGameMenu from './game/profile/ProfileIcon';
 
 export default function Home() {
   return (
     <div>
       <LandingPage/>
+      {/* <MainGameMenu/> */}
     </div>
   );
 }
